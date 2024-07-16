1ec9ba4a3e1283b1a8b8a8bda78249e55453bf02
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-16 11:14:39 +0000

    add FollowerList and url mapping

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Follower
+from .serializers import FollowerSerializer
+from memovault_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Follower.objects.filter(owner=self.request.user)
+        else:
+            return Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
