4a5d64815b6729e01944715bb49c35c141f24441
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-16 08:04:35 +0000

    add views, serializer and url mapping for the profile list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProfileSerializer
+from .models import Profile
 
 # Create your views here.
+
+
+class ProfileList(generics.ListAPIView):
+
+
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
