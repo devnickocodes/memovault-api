b032a5bc7a21e99de3a62bdd824f219261c48567
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-16 08:58:28 +0000

    add PostList view and url mapping for it

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
