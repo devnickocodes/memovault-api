8e040f7ff850e8c2fdb83b1d2d96fee9bb372646
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-16 10:57:10 +0000

    create followers app and add the follower model

---
+++ followers/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Follower(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def clean(self):
+        if self.owner == self.followed:
+            raise ValidationError("A user cannot follow themselves.")
+
+    def __str__(self):
+
+        return f"{self.owner} {self.followed}" 
