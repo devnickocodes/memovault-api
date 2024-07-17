7334e7ee7f40906b4663501361a066bc70027dfe
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-17 08:27:53 +0000

    add IsAdmin permission

--- memovault_api/permissions.py
+++ memovault_api/permissions.py
@@ -5,3 +5,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and request.user.is_staff
