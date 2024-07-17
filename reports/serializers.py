4ce14ad183240d3dc82f15d13f6d6cdee037b9c9
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-17 08:21:57 +0000

    add validation to the reports serializer

--- reports/serializers.py
+++ reports/serializers.py
@@ -9,5 +9,10 @@ class ReportSerializer(serializers.ModelSerializer):
         model = Report
         fields = ['id', 'owner', 'post', 'post_title', 'reason', 'custom_reason', 'created_at', 'updated_at']
 
+    def validate(self, data):
+        if data['reason'] == 'other' and not data.get('custom_reason'):
+            raise serializers.ValidationError("A reason is required when 'Other' is selected.")
+        return data
+
     def create(self, validated_data):
-            return Report.objects.create(**validated_data)
+        return Report.objects.create(**validated_data)
