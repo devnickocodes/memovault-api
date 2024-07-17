c175d5aa2f68c1fd679910c8f77e105c31abcba1
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-17 08:10:05 +0000

    add ReportDetailView and url mapping

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,5 @@ from reports import views
 
 urlpatterns = [
     path('reports/', views.ReportListCreate.as_view()),
+    path('reports/<int:pk>/', views.ReportDetailView.as_view()),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import NotAuthenticated
 from .models import Report
 from .serializers import ReportSerializer
 
@@ -11,3 +12,16 @@ class ReportListCreate(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class ReportDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReportSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Report.objects.filter(owner=self.request.user)
+        else:
+            raise NotAuthenticated("You must be logged in to access this report.")
+
+
