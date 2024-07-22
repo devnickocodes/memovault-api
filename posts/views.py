c2bdd34bc3faabb6508e1c948b5688ad73e7be37
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-22 06:47:21 +0000

    install django filters and add filterset_fields to posts views

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from memovault_api.permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
 from .models import Post
@@ -17,7 +18,14 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
             filters.OrderingFilter,
             filters.SearchFilter,
+            DjangoFilterBackend,
         ]
+    
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__profile'
+    ]
+
     ordering_fields  = [
         'comments_count',
         'post_likes_count',
