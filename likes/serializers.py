8c1984d19a798be7207b5433770c693398a35534
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-23 08:33:05 +0000

    add docstrings to likes/serializers.py

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,6 +2,18 @@ from rest_framework import serializers
 from .models import PostLike, CommentLike
 
 class PostLikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer which handles the representation and validation of `PostLike` instances.
+    
+    Attributes:
+        owner (ReadOnlyField): The username of the user who liked the post.
+        post (ForeignKey): The post that was liked.
+        created_at (DateTimeField): The timestamp when the like was created.
+
+    Methods:
+        create(validated_data): Handles the creation of a new `PostLike` instance,
+        and raises a validation error if a duplicate entry is detected.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
@@ -15,6 +27,18 @@ class PostLikeSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'detail': 'possible duplicate'})
 
 class CommentLikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer which handles the representation and validation of `CommentLike` instances.
+    
+    Attributes:
+        owner (ReadOnlyField): The username of the user who liked the comment.
+        comment (ForeignKey): The comment that was liked.
+        created_at (DateTimeField): The timestamp when the like was created.
+
+    Methods:
+        create(validated_data): Handles the creation of a new `CommentLike` instance,
+        and raises a validation error if a duplicate entry is detected.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
