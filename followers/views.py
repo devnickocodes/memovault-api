cea5c8dd25097c673d89856087a36077c338edb4
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-23 07:36:59 +0000

    remove the overrided get_queryset from FollowerList

--- followers/views.py
+++ followers/views.py
@@ -7,12 +7,6 @@ class FollowerList(generics.ListCreateAPIView):
     serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return Follower.objects.filter(owner=self.request.user)
-        else:
-            return Follower.objects.all()
-
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
