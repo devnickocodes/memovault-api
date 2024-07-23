70990ed2d425fe5db10e44038bf462fa3bc90d88
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-23 10:40:49 +0000

    add a docstring to profiles/models.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-# Create your models here.
+
 
 class Profile(models.Model):
+    """
+    Represents a user profile associated with :model:`auth.User`.
+
+    Attributes:
+        owner (ForeignKey): The owner of the profile.
+        created_at (DateTimeField): The timestamp when the profile was created.
+        updated_at (DateTimeField): The timestamp when the profile was last updated.
+        name (CharField): The name of the user (optional).
+        hobbies (CharField): The hobbies of the user (optional).
+        bio (TextField): A short biography of the user (optional).
+        image (ImageField): Profile image of the user (optional).
+    
+    Methods:
+        create_profile: Signal handler function that creates a profile when a new user is created.
+    
+    Meta:
+        ordering (list): Orders profiles by the `created_at` field in descending order.
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
