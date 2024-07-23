9e4dfcab6e25a78963834892f74971fdafe30a8d
Nikolay Hristev <devnickocodes@gmail.com>
2024-07-23 08:00:06 +0000

    add docstrings to likes/models.py

--- likes/models.py
+++ likes/models.py
@@ -4,6 +4,19 @@ from posts.models import Post
 from comments.models import Comment
 
 class PostLike(models.Model):
+    """
+    Stores a single like entry related to :model:`auth.User`.
+    And :model:`posts.Post`.
+
+    Attributes:
+        owner (ForeignKey): The user who liked the post.
+        post (ForeignKey): The post that was liked.
+        created_at (DateTimeField): The timestamp when the like was created.
+
+    Meta:
+        ordering (list): Orders records by the `created_at` field in descending order.
+        unique_together (list): Ensures that each combination of `owner` and `post` is unique.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,6 +29,19 @@ class PostLike(models.Model):
         return f"{self.owner} likes {self.post}"
 
 class CommentLike(models.Model):
+    """
+    Stores a single like entry related to :model:`auth.User`.
+    And :model:`comments.Comment`.
+
+    Attributes:
+        owner (ForeignKey): The user who liked the comment.
+        comment (ForeignKey): The comment that was liked.
+        created_at (DateTimeField): The timestamp when the like was created.
+
+    Meta:
+        ordering (list): Orders records by the `created_at` field in descending order.
+        unique_together (list): Ensures that each combination of `owner` and `comment` is unique.
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
