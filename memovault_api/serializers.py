fc3da5db7c41f0517afefce01ddfae080c5a2e30
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-15 11:28:48 +0000

    add a method to set the value of is_admin

--- memovault_api/serializers.py
+++ memovault_api/serializers.py
@@ -1,13 +1,18 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
-
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    is_admin = serializers.ReadOnlyField(source='is_staff')
+    is_admin = serializers.SerializerMethodField()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'is_admin', 'profile_id', 'profile_image'
         )
+    
+    def get_is_admin(self, obj):
+        """
+        Returns whether the user is an admin.
+        """
+        return obj.is_staff
