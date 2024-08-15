30523cafccd8d85efd2618899ad3cf3c6d47a3d7
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-15 11:02:28 +0000

    adjust CurrentUserSerializer to include is_admin field

--- memovault_api/serializers.py
+++ memovault_api/serializers.py
@@ -5,8 +5,9 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_admin = serializers.ReadOnlyField(source='is_staff')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'is_admin', 'profile_id', 'profile_image'
         )
